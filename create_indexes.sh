c3260a72f2fb300053d15a070151762a8b13b303
Colm McHugh <colm@couchbase.com>
2015-05-20 21:12:26 +0100

    Add sripts for creating, loading and indexing of buckets

---
+++ create_indexes.sh
@@ -0,0 +1,9 @@
+# create_indexes.sh
+
+for b in contacts customer reviews product purchases user_profile
+do
+	qry='statement=CREATE PRIMARY INDEX ON '
+	qry+=$b
+	qry+=' USING GSI'
+	curl http://127.0.0.1:8093/query/service -XPOST -d "$qry"
+done
---
+++ recreate_buckets.sh
@@ -0,0 +1,13 @@
+
+#Delete and create buckets
+for b in contacts customer reviews product purchases user_profile
+do
+    echo "Deleting bucket $b..."
+        curl -XDELETE http://127.0.0.1:8091/pools/default/buckets/$b -u Administrator:password
+    qry='name='
+    qry+=$b
+    let "port += 1"
+    echo "Creating bucket $b..."
+    curl http://127.0.0.1:8091/pools/default/buckets -XPOST -d "$qry" -u Administrator:password  -v  -d authType=none -d proxyPort=$port  -d ramQuotaMB=200
+done
+echo "Done"
