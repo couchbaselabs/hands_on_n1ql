67b179fe32f0b4ce49abcf43c758bd099e3f78d2
Colm McHugh <colm.mchugh@gmail.com>
2015-05-22 11:15:26 +0000

    Add: hands_on_n1ql: Ubuntu executable install_couchbase.sh: script for install couchbase load_data.sh script for loading and initializing data

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-echo go build
-go build
+./recreate_buckets.sh password
+tar zxf data.tar.gz
+./load_data.sh password
+rm -rf contacts customer reviews product purchases user_profile
+chmod +x create_indexes.sh
+./create_indexes.sh
+./hands_on_n1ql &
--- load_data.sh
+++ load_data.sh
@@ -7,5 +7,5 @@
 for b in contacts customer reviews product purchases user_profile
 do
     echo "Populating bucket $b"
-    cbdocloader -u Administrator -p $1 -n 127.0.0.1:8091 -b $b -s 100 $b
+    /opt/couchbase/bin/cbdocloader -u Administrator -p $1 -n 127.0.0.1:8091 -b $b -s 100 $b
 done
