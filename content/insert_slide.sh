e8a097490ffc90f8483bc05584ca7a8d26d23aab
Colm <colm@couchbase.com>
2015-05-28 18:01:06 +0100

    Update insert_slide.sh

--- content/insert_slide.sh
+++ content/insert_slide.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# START is the number of the new slide
-# END is the number of the last slide.
+# START is the number of where the new slide will go.
+# END is the number of the current final slide.
 
-# Given 47 slides, to insert a slide at 41, run:
+# To insert a slide at 41, given a 47 slide total, run this script as follows:
 # 	./insert_slide.sh 41 47
-# This will copy slide-47.md to slide-48.md, slide-46.md to slide-47.md, etc 
-# stopping at 41
+# This will copy slide-47.md to slide-48.md, slide-46.md to slide-47.md, etc stopping after copying slide-41.md to slide-42.md.
+# 
+# Go ahead and edit slide-41.md as desired (slide-42.md now contains the old slide-41.md)
+# 
 
 START=$1
 END=$2
